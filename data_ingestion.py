1ba117775caf7fa59d096d02098ace97467975d9
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-09-18 16:31:24 +0300

    db_connection test for loading

--- data_ingestion.py
+++ data_ingestion.py
@@ -6,6 +6,7 @@ import numpy as np
 import constants
 from kaggle.api.kaggle_api_extended import KaggleApi
 import psycopg2
+from db_config import db_config
 
 
 ## we start with ingesting our dataset from kaggle
@@ -30,6 +31,20 @@ print('download successful')
 
 
 
+#here we setup our connection
+conn = psycopg2.connect(
+    host = db_config['host'],
+    database = db_config['database'],
+    port = db_config['port'],
+    user = db_config['user'],
+    password = db_config['password']
+)
 
+cur = conn.cursor()
+
+test = cur.execute(
+    "SELECT * FROM information_schema.tables LIMIT 5"
+    )
+#now we 
 
     
